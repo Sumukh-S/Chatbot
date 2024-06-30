81f7fff4834fb0376edd20edb196133b2a54b2d1
Sumukh-S <sumukhs41041@gmail.com>
2024-07-01 00:38:17 +0530

    added starter files

---
+++ backend/src/index.ts
@@ -0,0 +1,12 @@
+import express from 'express'
+
+const app = express();
+
+// app open in local host 5000 port
+app.listen(5000,()=>console.log("Server open"));
+
+// app get request, where the / defines your localhost and if you put /hello it will open it as /hello infront of your localhost
+
+app.get('/', (req, res, next) => {
+  return res.send('Hello World');
+});
