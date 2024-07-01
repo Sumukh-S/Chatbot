52b6b2d90bcbed0bc942893e4cb6dabb00d37326
Sumukh-S <sumukhs41041@gmail.com>
2024-07-02 00:12:50 +0530

    chore: Update MongoDB connection string in .env file

---
+++ backend/src/db/connection.ts
@@ -0,0 +1,9 @@
+import {connect} from "mongoose";
+export default async function connectToDatabase() {
+    try{
+        await connect(process.env.MONGODB_URL);
+    } catch(error){
+        console.log(error);
+        throw new Error("Cannot connect to MongoDB")
+    }
+}
