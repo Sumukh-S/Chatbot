e93c9081329e0841a88255a3492240b2326b9085
Sumukh-S <sumukhs41041@gmail.com>
2024-07-03 22:43:17 +0530

    added routes and connnected to the MongoDB database

--- backend/dist/index.js
+++ backend/dist/index.js
@@ -1,26 +1,10 @@
-import express from 'express';
-import { config } from 'dotenv';
-config();
-const app = express();
-//app.use()
-app.use(express.json());
-// app open in local host 5000 port or CONNECTIONS AND LISTENERS
-app.listen(5000, () => console.log("Server open"));
-// // app get request, where the / defines your localhost and if you put /hello it will open it as /hello infront of your localhost
-// app.post('/', (req, res, next) => {
-//   console.log(req.body.name);
-//   return res.send('Hello World');
-// });
-// app.put('/', (req, res, next) => {
-//   console.log(req.body.name);
-//   return res.send('Hello World in put');
-// });
-// app.delete('/delete', (req, res, next) => {
-//   console.log(req.body.name);
-//   return res.send('Hello World in delete');
-// });
-// app.delete('/user/:userid', (req, res, next) => {
-//   console.log(req.params.userid);
-//   return res.send('Hello World in delete');
-// });
+import app from './app.js';
+import { connectToDatabase } from './db/connection.js';
+//connections and listeners
+const PORT = process.env.PORT || 5000;
+connectToDatabase()
+    .then(() => {
+    app.listen(PORT, () => console.log("Server Openn and Connected to Database😉!"));
+})
+    .catch((error) => console.log(error));
 //# sourceMappingURL=index.js.map
--- backend/src/db/connection.ts
+++ backend/src/db/connection.ts
@@ -1,9 +1,18 @@
-import {connect} from "mongoose";
-export default async function connectToDatabase() {
+import {connect, disconnect} from "mongoose";
+async function connectToDatabase() {
     try{
-        await connect(process.env.MONGODB_URL);
+        await connect(process.env.MONGODB_URL || '');
     } catch(error){
         console.log(error);
-        throw new Error("Cannot connect to MongoDB")
+        throw new Error("Cannot connect to MongoDB");
     }
 }
+async function disconnectFromDatabase() {
+    try{
+        await disconnect();
+    } catch(error){
+        console.log(error);
+        throw new Error("Cannot disconnect from MongoDB")
+    }
+}
+export {connectToDatabase, disconnectFromDatabase};
