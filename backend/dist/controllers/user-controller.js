5c0643f00e9742448b5d4e96395e4a5fcf03b74b
Sumukh-S <sumukhs41041@gmail.com>
2024-07-05 23:07:39 +0530

    TESTING API

--- backend/dist/controllers/user-controller.js
+++ backend/dist/controllers/user-controller.js
@@ -1,4 +1,12 @@
-export const getAllUsers = () => {
-    //get all user 
+import User from "../models/User.js";
+export const getAllUsers = async (req, res, next) => {
+    try {
+        const users = await User.find();
+        return res.status(200).json({ message: "OK for now", users });
+    }
+    catch (error) {
+        console.log(error);
+        return res.status(200).json({ message: "ERROR", cause: error.message });
+    }
 };
 //# sourceMappingURL=user-controller.js.map
--- backend/src/controllers/user-controller.ts
+++ backend/src/controllers/user-controller.ts
@@ -1,3 +1,12 @@
-export const getAllUsers = () => {
-    //get all user 
+import {NextFunction, Request, Response} from "express";
+import User from "../models/User.js";
+
+export const getAllUsers = async (req:Request, res:Response, next: NextFunction) => {
+    try {
+        const users = await User.find();
+        return res.status(200).json({message:"OK for now", users});
+    } catch (error: any) {
+        console.log(error);
+        return res.status(200).json({message:"ERROR", cause: error.message });  
+    }
 };
